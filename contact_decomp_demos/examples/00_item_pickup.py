f908396f5d0dfdcbebfae27e53c1ba584bdeaa96
David Alexander <dmalexa5@ncsu.edu>
2026-02-24 22:40:29 +0000

    Demo WIP

--- contact_decomp_demos/examples/00_item_pickup.py
+++ contact_decomp_demos/examples/00_item_pickup.py
@@ -19,7 +19,7 @@ print(robot.joint_values)
 
 # %%
 # Parameters for pickup location
-pickup_point = np.array([0.5, 0.0, 0.15])
+pickup_point = np.array([0.45, 0.0, 0.17])
 ctrl_freq = 50.0
 
 # %%
@@ -31,7 +31,7 @@ robot.cartesian_controller_parameters_client.load_param_config(
 # TODO: This is a hack to close and open the gripper
 # ros2 launch franka_gripper gripper.launch.py robot_ip:=<fci-ip>
 # to grip:
-# ros2 action send_goal -f /franka_gripper/grasp franka_msgs/action/Grasp "{width: 0.00, speed: 0.05, force: 50, epsilon: {inner: 0.05, outer: 0.05}}"
+# ros2 action send_goal -f /franka_gripper/grasp franka_msgs/action/Grasp "{width: 0.01, speed: 0.05, force: 50, epsilon: {inner: 0.05, outer: 0.05}}"
 # to stop:
 # ros2 service call /franka_gripper/stop std_srvs/srv/Trigger {}
 # to home:
@@ -40,8 +40,10 @@ robot.cartesian_controller_parameters_client.load_param_config(
 # %%
 input("Press Enter to go to the pickup point... ")
 robot.move_to(position=pickup_point, speed=0.15)
-
+robot.wait_until_ready()
 # %%
 input("Press Enter to pick up the item... ")
-robot.move_to(position=homing_pose.position, speed=0.15)
+next_point = np.array([0.45, 0.0, 0.18])
+robot.move_to(position=next_point, speed=0.15)
+robot.wait_until_ready()
 
